19c52e639e8a6b7c727bb581b6f5cd6f8f2ed73a
Anton Ångström <antonangstrom@gmail.com>
2017-06-02 16:06:36 +0200

    add basic GUI components

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,14 +1,19 @@
-//import
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Antons on 2017-02-27.
  */
 
-public class GUI {
+public class GUI extends JFrame {
 	
 	public GUI() {
 		
-		
+		setLayout(new BorderLayout());
+		setSize(400, 960);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 		
 	}
 
