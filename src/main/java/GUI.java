3280cbab3dab92f993423b042d293c1472817d94
Anton Ångström <antonangstrom@gmail.com>
2017-06-05 15:27:16 +0200

    new GUI components

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Antons on 2017-02-27.
@@ -14,6 +17,24 @@ public class GUI extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		JPanel nordpanel = new JPanel();
+		JPanel sydpanel = new JPanel();
+		
+		JButton gnrt = new JButton("Generate movies!");
+		final JTextArea print = new JTextArea();
+		
+		add(nordpanel, BorderLayout.NORTH);
+		nordpanel.add(gnrt);
+		//gnrt.addActionListener(new ActionListener() {
+		//	public void actionPerformed (ActionEvent aev) {
+		//		print.setText(Scraper.getResult());
+		//	}
+		//});
+		
+		
+		add(sydpanel, BorderLayout.SOUTH);
+		sydpanel.add(print);
 		
 	}
 
